d11b588fc6efcdecf2441b555909a3f8fc568f97
abeltje1 <abelvanhoek@gmail.com>
2017-03-17 17:51:32 +0100

    Update carrierwave-meta.rb

--- lib/carrierwave-meta.rb
+++ lib/carrierwave-meta.rb
@@ -1,6 +1,5 @@
 require 'active_support/concern'
 require 'active_support/core_ext/module/attribute_accessors'
-require 'mime/types'
 require 'carrierwave-meta/version'
 require 'carrierwave-meta/model_delegate_attribute'
 require 'carrierwave-meta/meta'
